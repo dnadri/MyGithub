c497be608c1df55f38d9268952e854ebe29cd52b
David Nadri <davidnadri@gmail.com>
2016-06-23 01:24:16 -0400

    Laid out IssueDetail view UI and data model

--- MyGithub/MyGithub/IssueDetailTableViewController.swift
+++ MyGithub/MyGithub/IssueDetailTableViewController.swift
@@ -7,16 +7,20 @@
 //
 
 import UIKit
+import Alamofire
 
 class IssueDetailTableViewController: UITableViewController {
 
-    var issue: Issue?
+    var issue: Issue!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("IssueDetailTableViewController: viewDidLoad() called.")
         
+        self.title = "Issue #\(issue.number!)"
+        
+        getIssueComments()
         
         // Self-sizing cells (auto-layout constraints must be set for cell)
         tableView.estimatedRowHeight = 80
@@ -29,6 +33,46 @@ class IssueDetailTableViewController: UITableViewController {
         
     }
     
+    func getIssueComments() {
+        
+        print("getIssueDetail() called.")
+        print("issue: \(issue.commentsURL!)")
+        
+//        let urlString = issue?.commentsURL!
+//        Alamofire.request(.GET, urlString!).validate(statusCode: 200..<300).responseJSON { response in
+//            print(response.request)  // original URL request
+//            print(response.response) // URL response
+//            print(response.data)     // server data
+//            print(response.result)   // result of response serialization
+//            
+//            if let json = response.result.value {
+//                print("***json: \(json)")
+//                print("***json.count: \(json.count)")
+//                
+//                for item in json as! [AnyObject] {
+//                    print("item in JSON: \(item)")
+//                    
+//                    let issue = Issue(number: item["number"] as? Int, title: item["title"] as? String, body: item["body"] as? String, comments: item["comments"] as? Int, commentsURL: item["comments_url"] as? String, timestamp: item["updated_at"] as? String, state: item["state"] as? String)
+//                    
+//                    self.issues.append(issue)
+//                    
+//                    
+//                }
+//                
+//                self.tableView.reloadData()
+//                
+//            } else {
+//                
+//                print("ERROR: \(response.result.error)")
+//                let alert = UIAlertController(title: "Error", message: "\(response.result.error)", preferredStyle: .Alert)
+//                alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+//                alert.presentViewController(alert, animated: true, completion: nil)
+//                
+//            }
+//            
+//        }
+        
+    }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
@@ -44,33 +88,25 @@ class IssueDetailTableViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("IssueDetailCell") as! IssueDetailTableViewCell
         
-//        if let number = self.issues[indexPath.row]?.number {
-//            cell.numberLabel.text = "#\(number)"
-//        }
-//        
-//        if let title = self.issues[indexPath.row]?.title {
-//            cell.titleLabel.text = title
-//        }
-//        
-//        // "Updated Jun 13, 2016, 6:26PM EDT"
-//        // i.e.: "Updated 4 days ago"
-//        if let dateUpdated = self.issues[indexPath.row]?.timestamp {
-//            let dateFormatter = NSDateFormatter()
-//            // The format must match the timestamp string otherwise it will return nil
-//            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:sszzz"
-//            let date = dateFormatter.dateFromString(dateUpdated)
-//            cell.timestampLabel.text = NSDate().offsetFrom(date!)
-//        }
-//        
-//        if let state = self.issues[indexPath.row]?.state {
-//            if state == "closed" {
-//                // Ideal to create constants for things like image names, but in the interest of time...
-//                cell.stateImageView.image = UIImage(named: "closed")
-//            } else {
-//                cell.stateImageView.image = UIImage(named: "opened")
-//            }
-//        }
+        cell.numberLabel.text = "#\(issue.number!)"
         
+        cell.titleLabel.text = issue.title!
+        
+        cell.bodyLabel.text = issue.body!
+        
+        // i.e.: "Updated 4 days ago"
+        let dateFormatter = NSDateFormatter()
+        // The format must match the timestamp string otherwise it will return nil
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:sszzz"
+        let date = dateFormatter.dateFromString(issue.timestamp!)
+        cell.timestampLabel.text = NSDate().offsetFrom(date!)
+        
+        if issue.state! == "closed" {
+            // Ideal to create constants for things like image names, but in the interest of time...
+            cell.stateImageView.image = UIImage(named: "closed")
+        } else {
+            cell.stateImageView.image = UIImage(named: "opened")
+        }
         
         return cell
         
